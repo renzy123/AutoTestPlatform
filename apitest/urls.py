2e81af874973555cb5160c4547b6f275ce70b719
Reid Ren <renzy1989910@gmail.com>
2019-06-15 19:37:55 +0800

    1、借口测试GET请求完成

--- apitest/urls.py
+++ apitest/urls.py
@@ -4,4 +4,5 @@ from apitest import views
 
 urlpatterns = [
     path('index/', views.init_apitest_page, name="index"),
+    path('sendRequest/', views.handle_api_request, name="request"),
 ]
--- loadtest/views.py
+++ loadtest/views.py
@@ -3,9 +3,13 @@
 from django.shortcuts import render
 
 
+
+
 def initiate_loadtest_page(request):
     # 初始化压力测试页面
     if request.method == "GET":
         return render(request, "pages/jjMatch/loadtest.html")
         pass
     pass
+
+
